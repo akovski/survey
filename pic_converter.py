5f4f441207231cc314a5dc97e2ebccf61c408aa8
akovski <akovskiboy@gmail.com>
2016-04-15 16:11:42 +0800

    add group info

--- pic_converter.py
+++ pic_converter.py
@@ -6,18 +6,21 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', '-i', required=True)
-    parser.add_argument('--output', '-o', required=True)
     args = parser.parse_args()
-    if not os.path.exists(args.output):
-        os.makedirs(args.output)
-    for cnt, pic in enumerate(sorted(glob.glob(os.path.join(args.input, '*')))):
+
+    pics = [os.path.join(root, file) for root, dirs, files in os.walk(args.input) for file in files
+            if file[-4:] == '.jpg']
+
+    for pic in pics:
+        print pic, pic.replace('-000', '.')
+        os.rename(pic, pic.replace('-000', '.'))
+        continue
         print pic
-        dirname, basename = os.path.split(pic)
         img = cv2.imread(pic)
         ox, oy = img.shape[:2]
         ox /= 2
         oy /= 2
         img = img[ox-350:ox+350, oy-350:oy+350]
-        #cv2.imwrite(os.path.join(args.output, basename.replace('.png', '.jpg')), img)
-        cv2.imwrite(os.path.join(args.output, '{}.{}.jpg'.format(cnt / 6 + 1, cnt % 6 + 1)), img)
+        cv2.imwrite(pic[:-4] + '.jpg', img)
+        os.remove(pic)
 
--- server.py
+++ server.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
 import glob
+import random
 import os.path
 from flask import Flask, render_template, make_response, request, redirect, url_for
 app = Flask(__name__)
 
-userlist = dict()
-datalist = []
+userlist = dict() #keep record of current progress of all users
+datalist = [] 
+group = []
+max_people = 6
+max_group  = 4
+
+# 24 people * 600 = (3 * 800) * 6
+# 4 stages, each 600 pics
 
 def save_user(user):
     with open(os.path.join('users', user), 'w') as f:
         f.write(str(userlist[user]))
 
-def load_datalist(datafile):
+def load_data(datafile):
     with open(datafile, 'r') as f:
         return [x.strip().split() for x in f.readlines()]
+
+def load_datalist():
+    return [load_data('data{}.txt'.format(i + 1)) for i in range(max_group)]
 
 def load_users():
     users = dict()
@@ -24,13 +34,38 @@ def load_users():
             users[name] = int(f.read()) 
     return users
 
+def save_group():
+    with open('group.txt', 'w') as f:
+        for g in group:
+            print >>f, ' '.join(g)
+
+def load_group(groupfile):
+    res = []
+    with open(groupfile, 'r') as f:
+        for line in f.readlines():
+            res.append(f.strip().split())
+    return res
+
 def new_user(user):
     global userlist
+    x = 0
+    while x < len(group):
+        if len(group[x]) < max_people:
+            group[x].append(user)
+    if x == len(group):
+        group.append([user])
+    save_group()
     userlist[user] = 0
     save_user(user)
 
+def get_datalist(user):
+    if i, g in enumerate(group):
+        if user in g:
+            return datalist[i]
+
 def get_pair(user):
-    return datalist[userlist[user]]
+    return get_datalist(user)[userlist[user]]
+
 
 @app.route('/')
 def index():
@@ -40,9 +75,10 @@ def index():
     if userlist[name] == len(datalist):
         return render_template('end.html')
     pair = get_pair(name)
+    random.shuffle(pair)
     return render_template('index.html', left_id=pair[0], right_id=pair[1])
 
-def log_choice(name, a, b):
+def log_choice(name, a, b): #choice a over b
     print name, a, b
     #with open(os.path.join('./logs', name+'.txt'), 'a') as f:
         #print >>f, a, b
@@ -65,7 +101,7 @@ def choose():
 @app.route('/login', methods=['POST'])
 def login():
     name = request.form['name']
-    if len(name) < 4 or len(name) > 20:
+    if len(name) < 4 or len(name) > 20 or (not name.isalnum()):
         return render_template('login.html')
     if name not in userlist:
         new_user(name)
@@ -82,5 +118,5 @@ def clear():
 if __name__ == '__main__':
     datalist = load_datalist('data.txt')
     userlist = load_users()
-    print userlist, datalist
+    group    = load_group('group.txt')
     app.run(host='0.0.0.0',port=8888,debug=True)
