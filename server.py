cab34b4c294df47a45fbdbf67a7b9cbed019b712
akovski <akovskiboy@gmail.com>
2016-04-18 14:10:46 +0800

    random shuffled order, add progress bar

--- server.py
+++ server.py
@@ -2,11 +2,13 @@
 import glob
 import random
 import os.path
+import argparse
 from flask import Flask, render_template, make_response, request, redirect, url_for
 app = Flask(__name__)
 
 userlist = dict() #keep record of current progress of all users
-datalist = [] 
+datalist = dict()
+groupdata = [] 
 group = []
 max_people = 6
 max_group  = 4
@@ -17,19 +19,28 @@ max_group  = 4
 def save_user(user):
     with open(os.path.join('users', user), 'w') as f:
         f.write(str(userlist[user]))
+    with open(os.path.join('userdata', user+'.txt'), 'w') as f:
+        for x, y in datalist[user]:
+            print >>f, x, y
 
 def load_data(datafile):
     with open(datafile, 'r') as f:
         return [x.strip().split() for x in f.readlines()]
 
 def load_datalist():
+    res = dict()
+    for user in userlist:
+        res[user] = load_data(os.path.join('userdata', user+'.txt'))
+    return res
+
+def load_groupdata():
     return [load_data('data{}.txt'.format(i + 1)) for i in range(max_group)]
 
 def load_users():
     users = dict()
     for userfile in sorted(glob.glob('./users/*')):
         name = os.path.split(userfile)[-1]
-        print name
+        #print name
         with open(userfile, 'r') as f:
             users[name] = int(f.read()) 
     return users
@@ -43,25 +54,35 @@ def load_group(groupfile):
     res = []
     with open(groupfile, 'r') as f:
         for line in f.readlines():
-            res.append(f.strip().split())
+            res.append(line.strip().split())
     return res
 
 def new_user(user):
     global userlist
     x = 0
     while x < len(group):
+        if user in group[x]:
+            break
         if len(group[x]) < max_people:
             group[x].append(user)
+            break
+        x += 1
     if x == len(group):
         group.append([user])
-    save_group()
+    data = [[x, y] for x, y in groupdata[x]]
+    random.seed(user)
+    random.shuffle(data)
+    datalist[user] = data
     userlist[user] = 0
+    with open(os.path.join('userdata', user+'.txt'), 'w') as f:
+        for x, y in datalist[user]:
+            print >>f, x, y
+    save_group()
     save_user(user)
+    
 
 def get_datalist(user):
-    if i, g in enumerate(group):
-        if user in g:
-            return datalist[i]
+    return datalist[user]
 
 def get_pair(user):
     return get_datalist(user)[userlist[user]]
@@ -72,16 +93,18 @@ def index():
     if 'name' not in request.cookies or request.cookies['name'] not in userlist:
         return render_template('login.html')
     name = request.cookies['name']
-    if userlist[name] == len(datalist):
+    if userlist[name] == len(get_datalist(name)):
         return render_template('end.html')
     pair = get_pair(name)
     random.shuffle(pair)
-    return render_template('index.html', left_id=pair[0], right_id=pair[1])
+    return render_template('index.html', left_id=pair[0], right_id=pair[1], 
+            progress= (userlist[name] * 100 + len(get_datalist(name)) - 1) / len(get_datalist(name)))
 
 def log_choice(name, a, b): #choice a over b
     print name, a, b
-    #with open(os.path.join('./logs', name+'.txt'), 'a') as f:
-        #print >>f, a, b
+    if not app.debug:
+        with open(os.path.join('./logs', name+'.txt'), 'a') as f:
+            print >>f, a, b
     userlist[name] = userlist[name] + 1
     save_user(name)
 
@@ -116,7 +139,16 @@ def clear():
     return response
 
 if __name__ == '__main__':
-    datalist = load_datalist('data.txt')
+    groupdata = load_groupdata()
+    print len(groupdata)
     userlist = load_users()
+    print userlist
+    datalist = load_datalist()
     group    = load_group('group.txt')
-    app.run(host='0.0.0.0',port=8888,debug=True)
+    print group
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+
+    app.run(host='0.0.0.0',port=8888, debug=args.debug)
